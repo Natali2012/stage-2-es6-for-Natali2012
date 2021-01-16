e00b01e6e2c3c74d98afb4a732f4b675f2c3dcce
Natali2012 <77192040+Natali2012@users.noreply.github.com>
2021-01-17 00:00:02 +0200

    Get data from remote API using fetch

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ import App from './src/javascript/app';
 import './src/styles/styles.css';
 
 new App();
+const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+
+fetch(API_URL)
+  .then(response => response.json())
+  .then(file => {
+    const fighters = JSON.parse(atob(file.content));
+    console.log(fighters);
+  });
